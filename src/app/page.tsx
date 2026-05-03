6e11862c97962050eab87a18b3229e25111e5ed3
성태경 <taegyeong0320@naver.com>
2026-05-04 00:24:31 +0900

    fix: 홈 페이지 데모 코드 제거

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,3 @@
-import { PlusIcon } from '@/components/common/icons/PlusIcon';
-import { SearchBar } from '@/components/common/SearchBar';
-import { TextField } from '@/components/common/TextField';
-import { Button } from '@/components/ui/button';
-
 export default function Home() {
-  return (
-    <div>
-      Home
-      <Button leftIcon={<PlusIcon />}>gdgdgdg</Button>
-      <TextField />
-      <SearchBar />
-    </div>
-  );
+  return <div>Home</div>;
 }
