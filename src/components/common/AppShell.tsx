b373938c147c3796fd570b21f60953079ed4e268
성태경 <taegyeong0320@naver.com>
2026-05-08 16:43:35 +0900

    feat: 앱 콘텐츠 기준 공통 모달 구현

--- src/components/common/AppShell.tsx
+++ src/components/common/AppShell.tsx
@@ -7,10 +7,23 @@ import { cn } from '@/lib/utils';
 
 export function AppShell({ children }: { children: React.ReactNode }) {
   const [collapsed] = React.useState(false);
+  const sidebarWidth = collapsed ? '73px' : '252px';
+  const appContentLeft = collapsed ? '89px' : '268px';
+
+  React.useEffect(() => {
+    document.documentElement.style.setProperty('--app-sidebar-width', sidebarWidth);
+    document.documentElement.style.setProperty('--app-content-left', appContentLeft);
+
+    return () => {
+      document.documentElement.style.removeProperty('--app-sidebar-width');
+      document.documentElement.style.removeProperty('--app-content-left');
+    };
+  }, [appContentLeft, sidebarWidth]);
 
   return (
     <div
       className={cn(
+        'min-h-full p-4',
         // sidebar-width는 사이드바 자체 너비, content-left는 left-4 여백까지 포함한 콘텐츠 시작 위치
         collapsed
           ? '[--app-content-left:89px] [--app-sidebar-width:73px]'
